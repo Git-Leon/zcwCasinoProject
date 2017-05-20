c200d631e2b8642f46bcad3e308d10d801608d7a
Leon <leon@Leons-MacBook-Pro.local>
2017-05-19 20:24:53 -0400

    modified `TestProfile` class

--- src/test/java/leon/casino/TestProfile.java
+++ src/test/java/leon/casino/TestProfile.java
@@ -1,10 +1,16 @@
 package leon.casino;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestProfile {
-    Profile testProfile = new Profile("TestProfile", 99999999);
+    private Profile testProfile;
+
+    @Before
+    public void setup() {
+        this.testProfile = new Profile("TestProfile", 99999999);
+    }
 
     @Test
     public void testGetBalance() {
